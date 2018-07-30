45dd6a8edede0ed942596b9cb5f86fe9bf1254df
cyx18844131633 <41895729+cyx18844131633@users.noreply.github.com>
2018-07-30 18:06:14 +0800

    Add files via upload

---
+++ Figure_1/MainPlot_Figure_1.m
@@ -0,0 +1,9 @@
+FileName={ 'abc_Perb_10.mat','abc_Perb_20.mat',...
+          'abc_Perb_30.mat','abc_Perb_40.mat','abc_Perb_50.mat'};
+Leg={'1','2','3','4','5'};
+
+%% »­Îó²îÇúÏß
+% ResultPlotErrors( FileName ,1,Leg);
+
+%% »­ÏìÓ¦ÇúÏß
+ResultPlot_Fig_1(FileName,1,Leg);
